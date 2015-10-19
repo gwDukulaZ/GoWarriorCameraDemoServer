40d03b250b5a6f0048aea7fda95f28985ef707c4
GoWarrior <support@gowarriorosh.com>
2015-10-19 17:24:44 +0800

    delete qccode menu code

--- app/src/main/java/com/gowarrior/camera/server/QCCode.java
+++ app/src/main/java/com/gowarrior/camera/server/QCCode.java
@@ -2,8 +2,6 @@ package com.gowarrior.camera.server;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class QCCode extends Activity {
@@ -14,25 +12,4 @@ public class QCCode extends Activity {
         setContentView(com.gowarrior.camera.server.R.layout.activity_qccode);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(com.gowarrior.camera.server.R.menu.menu_qccode, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == com.gowarrior.camera.server.R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
